3222ddc748649a613c12272712a8d9f90bf64720
ioana chelu <ioana.chelu28@gmail.com>
2016-04-08 17:59:36 +0300

    changed perplexity and scaled results

--- plot_tsne.py
+++ plot_tsne.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 
 vocab_path = "./output/vocab.txt"
 tsne_path = "./output/tsne.txt"
+scale_factor = 100000
 
 with open(tsne_path, "r") as f:
   data_list = []
   for line in f:
     x, y = line.split("\t")
-    data_list.append([x,y])
-  data = np.asarray(data_list)
+    data_list.append([float(x) * scale_factor, float(y) * scale_factor])
+  data = np.asarray(data_list, dtype=np.float32)
 
 
 vocab_words = []
@@ -25,7 +26,7 @@ labels = ['%s' % vocab_words[i] for i in range(N)]
 
 # plt.subplots_adjust(bottom = 0.1)
 
-plt.scatter(data[:, 0], data[:, 1], color='b', alpha=.4, s=1)
+plt.scatter(data[:, 0], data[:, 1], color='b', alpha=0, s=1)
 
 for label, x, y in zip(labels, data[:, 0], data[:, 1]):
   plt.annotate(
@@ -33,4 +34,4 @@ for label, x, y in zip(labels, data[:, 0], data[:, 1]):
       xy = (x, y), fontsize=1)
 
 # plt.show()
-plt.savefig('test2.pdf', format='pdf')
+plt.savefig('test.pdf', dpi=1200, format='pdf')
